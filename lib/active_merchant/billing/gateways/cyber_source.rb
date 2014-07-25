1cbf8c489bd4629c78be6988c47cf3bc250d331c
Martin Troels Eberhardt <martin@darkfox.dk>
2014-07-25 13:45:40 +0200

    Remove debugging code.

--- lib/active_merchant/billing/gateways/cyber_source.rb
+++ lib/active_merchant/billing/gateways/cyber_source.rb
@@ -592,11 +592,7 @@ module ActiveMerchant #:nodoc:
       # Contact CyberSource, make the SOAP request, and parse the reply into a
       # Response object
       def commit(request, options)
-        puts request
-        puts options
-        built_request = build_request(request, options)
-        puts built_request
-        response = parse(ssl_post(test? ? self.test_url : self.live_url, built_request))
+        response = parse(ssl_post(test? ? self.test_url : self.live_url, build_request(request, options)))
 
         success = response[:decision] == "ACCEPT"
         message = @@response_codes[('r' + response[:reasonCode]).to_sym] rescue response[:message]
